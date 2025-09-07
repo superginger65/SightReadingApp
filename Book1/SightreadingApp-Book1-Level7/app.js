483c30358ad73b09e3251544c1ce1866ea5fc76f
superginger65 <superginger65@gmail.com>
2025-09-06 22:14:03 -0400

    Added Metronome to Levels

--- Book1/SightreadingApp-Book1-Level7/app.js
+++ Book1/SightreadingApp-Book1-Level7/app.js
@@ -145,3 +145,35 @@ function generateActivity(meter) {
     }
   }
 }
+
+// Function to play or pause all tracks in sync
+  function togglePlayPause() {
+    const masterTrack = document.getElementById('metronome-audio');
+    const masterButton = document.getElementById('metronome-play-pause');
+
+    if (masterTrack.paused) {
+      masterTrack.play();
+      masterButton.textContent = '⏸︎';
+    }
+    else {
+      masterTrack.pause();
+      masterButton.textContent = '⏵︎';
+    }
+  }
+
+// Function to change the playback speed based on the input BPM
+function changeSpeed(bpm) {
+  const masterTrack = document.getElementById('metronome-audio');
+  
+  const baseBpm = 100;
+  const speed = bpm / baseBpm;
+  
+  // Adjust the playback speed
+  masterTrack.playbackRate = speed;
+}
+//Function to Reset the Speed to default
+function resetSpeed() {
+  const slider = document.getElementById('speed');
+  slider.value = 100; // Reset the slider to 100
+  changeSpeed(100); // Reset the speed to default
+}
